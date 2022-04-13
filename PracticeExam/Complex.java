dc58b06d22c7e14c2a1e9f3489b8097f43a8ea45
NguyetCam <vuchaunguyetcam@gmail.com>
2022-04-13 10:31:55 +0700

    Practice Exam
    JP1 - Semester 2

---
+++ PracticeExam/Complex.java
@@ -0,0 +1,65 @@
+package PracticeExam;
+
+public class Complex {
+    public Double realPart,imaginaryPart;
+
+    public Double getRealPart() {
+        return realPart;
+    }
+
+    public void setRealPart(Double realPart) {
+        this.realPart = realPart;
+    }
+
+    public Double getImaginaryPart() {
+        return imaginaryPart;
+    }
+
+    public void setImaginaryPart(Double imaginaryPart) {
+        this.imaginaryPart = imaginaryPart;
+    }
+
+    public Complex() {
+        Complex temp = new Complex(0.0,0.0);
+    }
+
+    public Complex(Double realPart, Double imaginaryPart) {
+        this.realPart = realPart;
+        this.imaginaryPart = imaginaryPart;
+    }
+
+    public Complex add(Complex otherNumber){
+        Complex result = new Complex();
+        result.realPart = this.realPart+otherNumber.realPart;
+        result.imaginaryPart = this.imaginaryPart+ otherNumber.imaginaryPart;
+        return result;
+    }
+
+    public Complex subtract(Complex otherNumber){
+        Complex result = new Complex();
+        result.realPart = this.realPart- otherNumber.realPart;
+        result.imaginaryPart = this.imaginaryPart-otherNumber.imaginaryPart;
+        return result;
+    }
+
+    public Complex multiply(Complex otherNumber){
+        Complex result = new Complex();
+        result.realPart = this.realPart*(otherNumber.realPart+otherNumber.imaginaryPart);
+        result.imaginaryPart = this.imaginaryPart*(otherNumber.realPart+ otherNumber.imaginaryPart);
+        return result;
+    }
+
+    public Complex divide(Complex otherNumber){
+        Complex result = new Complex();
+        result.realPart = (this.realPart*otherNumber.realPart + this.imaginaryPart* otherNumber.imaginaryPart)/(Math.pow(otherNumber.realPart,2)+Math.pow(otherNumber.imaginaryPart,2));
+        result.imaginaryPart = (this.imaginaryPart* otherNumber.realPart - this.realPart* otherNumber.imaginaryPart)/(Math.pow(otherNumber.realPart,2)+Math.pow(otherNumber.imaginaryPart,2));
+        return result;
+    }
+
+    public String toString(){
+        if(this.getImaginaryPart()<0){
+            return this.realPart+""+this.imaginaryPart+"*i";
+        }
+        return this.realPart+"+"+this.imaginaryPart+"*i";
+    }
+}
---
+++ PracticeExam/ComplexDemo.java
@@ -0,0 +1,15 @@
+package PracticeExam;
+
+public class ComplexDemo {
+    public static void main(String[] args){
+        Complex cp1 = new Complex(2.0,5.0),
+        add = cp1.add(new Complex(5.0,2.0)),
+        subtract = cp1.subtract(new Complex(2.0,1.0)),
+        multiply= cp1.multiply(new Complex(3.0,4.0)),
+        divide = cp1.divide(new Complex(1.0,4.0));
+        System.out.println("Sum: "+add.toString());
+        System.out.println("Subtraction: "+subtract.toString());
+        System.out.println("Multiplication: "+multiply.toString());
+        System.out.println("Division: "+divide.toString());
+    }
+}
